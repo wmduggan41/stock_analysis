b0c1212efc0a5620ffbc59173597476461103e76
Wmduggan41 <wmduggan@gmail.com>
2023-08-15 17:55:49 -0400

    project framework updated. local changes refactored

---
+++ skeleton_stock_analysis.R
@@ -0,0 +1,78 @@
+# BUSINESS SCIENCE ----
+# DS4B 202-R ----
+# STOCK ANALYZER APP - DATA ANALYSIS -----
+# Version 1
+
+# APPLICATION DESCRIPTION ----
+# - The user will select 1 stock from the SP 500 stock index
+# - The functionality is designed to pull the past 180 days of stock data
+# - Implement 2 moving averages - short (fast) and long (slow)
+# - Produce a timeseries visualization
+# - Produce automated commentary based on the moving averages
+
+# REPRODUCIBILITY REQUIREMENTS
+# - The functionality is designed to pull the past 180 days of stock data from today's date
+# - Because of this, your analysis may differ from mine
+# - To reproduce my analysis, replace today() with ymd("2023-07-19")
+
+# LIBRARIES ----
+library(plotly)
+library(tidyquant)
+library(tidyverse)
+library(fs)
+
+
+
+# 1.0 GET STOCK LIST ----
+# Created function by stringing together variable arguments for stock lists
+stock_list_tbl <- tq_index("SP500") %>% 
+  select(symbol, company) %>% 
+  arrange(symbol) %>% 
+  mutate(label = str_c(symbol, company, sep = ", ")) %>% 
+  select(label)
+
+stock_list_tbl
+
+get_stock_list <- function(stock_index = "SP500") {
+  tq_index(stock_index) %>% 
+    select(symbol, company) %>% 
+    arrange(symbol) %>% 
+    mutate(label = str_c(symbol, company, sep = ", ")) %>% 
+    select(label)
+  
+}
+
+tq_index_options()
+
+get_stock_list("DOW")
+
+get_stock_list("SP500")
+
+# 2.0 EXTRACT SYMBOL BASED ON USER INPUT ----
+
+user_input <- "AAPL, Apple Inc."
+
+user_input %>% str_split(pattern = ", ") %>% pluck(1, 1)
+
+get_symbol_from_user_input <- function(user_input) {
+  user_input %>% str_split(pattern = ", ") %>% pluck(1, 1)
+  
+}
+
+# 3.0 GET STOCK DATA ----
+
+
+
+# 4.0 PLOT STOCK DATA ----
+
+
+
+# 5.0 GENERATE COMMENTARY ----
+
+
+
+# 6.0 TEST WORKFLOW ----
+
+
+
+# 7.0 SAVE SCRIPTS ----
