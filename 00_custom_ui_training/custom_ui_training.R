3053365c91f40aedb8d15f7a2bfa7938ea7d7bd2
Wmduggan41 <wmduggan@gmail.com>
2024-01-04 12:46:43 -0500

    custom ui template added

---
+++ 00_custom_ui_training/custom_ui_training.R
@@ -0,0 +1,162 @@
+# BUSINESS SCIENCE ----
+# DS4B 202-R ----
+# CUSTOM UI TRAINING -----
+# Version 1
+
+# APPLICATION DESCRIPTION ----
+# - Make custom shiny functions for  Bootstrap 3 components: Jumbotron, Info Card, and Thumbnail
+
+# LIBRARIES ----
+# Shiny
+library(shiny)
+library(shinythemes)
+
+# Core
+library(tidyverse)
+
+# CUSTOM FUNCTIONS ----
+jumbotron <- function(..., background_img = NULL, ui_box_bg_color = "rgba(0,0,0,0.5)", ui_box_text_color = "white") {
+    
+    if (is.null(background_img)) {
+        style_jumbotron = ""
+    } else {
+        style_jumbotron <- str_glue("background-image:url({background_img}); background-size:cover;")
+    }
+    
+    div(
+        class = "jumbotron",
+        style = style_jumbotron,
+        
+        div(
+            class = "jumbotron-ui-box",
+            style = str_glue("color: {ui_box_text_color}; background-color: {ui_box_bg_color}; padding:25px;"),
+            
+            # User Inputs Go Here
+            ...
+            
+        )
+    )
+    
+}
+
+
+info_card <- function(title, value, sub_value,
+                      main_icon = "chart-line", sub_icon = "arrow-up",
+                      bg_color = "default", text_color = "default", sub_text_color = "success") {
+    
+    div(
+        class = "panel panel-default",
+        div(
+            class = str_glue("panel-body bg-{bg_color} text-{text_color}"),
+            p(class = "pull-right", icon(class = "fa-3x", main_icon)),
+            h4(title),
+            h5(value),
+            p(
+                class = str_glue("text-{sub_text_color}"),
+                icon(sub_icon),
+                tags$small(sub_value)
+            )
+        )
+    )
+    
+}
+
+
+# UI ----
+ui <- fixedPage(
+    title = "Custom UI Components",
+    
+    themeSelector(),
+    
+    h1(class = "page-header", "Custom UI Training"),
+    
+    # JUMBOTRON COMPONENT ----
+    div(
+        class = "container",
+        id = "jumbotron",
+        h2("Jumbotron Component"),
+        
+        column(
+            width = 12,
+            div(
+                class = "jumbotron",
+                style = "background-image:url('data_science_team.jpg'); background-size:cover;",
+                
+                div(
+                    class = "jumbotron-ui-box text-default bg-default",
+                    style = "color: white; background-color: rgba(0,0,0,0.5); padding:25px;",
+                    
+                    # User Inputs Go Here
+                    h1("Why Learn Shiny?", style="color:white;"),
+                    a(href = "#", class = "btn btn-primary", "Learn More")
+                    
+                )
+            )
+        ),
+        
+        column(
+            width = 12,
+            jumbotron(
+                background_img = "data_science_team.jpg",
+                ui_box_bg_color = "rgba(0,125,255,0.35)",
+                ui_box_text_color = "white",
+                
+                h1("Why Learn Shiny?"),
+                a(href = "#", class = "btn btn-primary", "Learn More")
+            )
+        )
+    ),
+    
+    # CARD COMPONENT ----
+    div(
+        class = "container",
+        id = "cards",
+        h2("Info Card"),
+        
+        column(
+            width = 4,
+            div(
+                class = "panel panel-default",
+                div(
+                    class = "panel-body bg-default text-default",
+                    p(class = "pull-right", icon(class = "fa-3x", "chart-line")),
+                    h4("AAPL"),
+                    h5("20-day <small>vs 50-Day</small>" %>% HTML()),
+                    p(
+                        class = "text-success",
+                        icon("arrow-up"),
+                        tags$small("20%")
+                    )
+                )
+            )
+        ),
+        
+        column(
+            width = 4,
+            
+            info_card(title = "NFLX", 
+                      value = p("20-Day", tags$small("vs 50-Day")), 
+                      sub_value = "-10%", sub_icon = "arrow-down", sub_text_color = "danger")
+        )
+        
+    ),
+    
+    # THUMBNAIL CHALLENGE ----
+    
+    div(
+        class = "container",
+        id    = "thumbnails",
+        h2("Thumbnails")
+    ),
+    
+    div(style = "height:400px;")
+    
+    
+)
+
+# SERVER ----
+server <- function(input, output, session) {
+    
+}
+
+shinyApp(ui, server)
